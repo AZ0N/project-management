4a6f4e42742cb1cd4bb9a3b166874effe3c9b505
tanner1399 <Jonathanflint@live.dk>
2023-04-13 13:28:34 +0200

    Create ProjectLeader.java

---
+++ src/main/java/application/ProjectLeader.java
@@ -0,0 +1,21 @@
+package application;
+
+public class ProjectLeader {
+	
+	private int ID;
+	private String Initials;
+
+	
+	public ProjectLeader(int ID, String Initials){
+		this.ID = ID;
+		this.Initials = Initials;
+	}
+	
+	public int getID() {
+		return ID;
+	}
+	
+	public String Initials() {
+		return Initials;
+	}
+}
