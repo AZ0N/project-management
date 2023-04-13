bbca1e284388238ac82714ef1fc9a1a0945b17c2
oscarsvenstrup <129071972+oscarsvenstrup@users.noreply.github.com>
2023-04-13 13:16:34 +0200

    Create Employees.java
    Employee class

---
+++ src/main/java/application/Employees.java
@@ -0,0 +1,29 @@
+package application;
+
+public class Employees {
+
+	private String initials;
+	private int numberOfActivities;
+
+	
+	public Employees(String initials) {
+		this.initials = initials;
+		
+	}
+	
+	public String getInitials() {
+		return initials;
+	}
+	
+	// public Calendar availability(){}
+	
+	// public List<PredefinedActivites> assignedPredefinedActivities(){}
+	
+	// public List<PorjectActivities> assignedProjectActivities(){}
+	
+	// public int dailyWorktime(){}
+
+
+}
+
+
