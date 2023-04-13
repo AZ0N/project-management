a7cac8e9e9d82b9de81934aefa595370d668bc50
s224818 <s224818@student.dtu.dk>
2023-04-13 16:02:54 +0200

    predefinedActivities
    done

--- src/main/java/application/PredefinedActivities.java
+++ src/main/java/application/PredefinedActivities.java
@@ -1,20 +1,53 @@
-package application;
-
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
+public class PredefinedActivities implements Activities {
+    private String name;
+    private int estimatedTime;
+    private Date startDate;
+    private Date endDate;
+    private ArrayList<String> causeOfAbsence;
 
-public class PredefinedActivities {
-	
-	private String predefinedActivity;
-	
-	private List<String> predefinedActivities = new ArrayList<>();
-	
-	public void predefinedActivities (String predefinedActivity) {
-		
-		this.predefinedActivity = predefinedActivity;
-		
-		predefinedActivities.add(predefinedActivity);
+    public PredefinedActivities(String name, int estimatedTime, Date startDate, Date endDate, ArrayList<String> causeOfAbsence) {
+        this.name = name;
+        this.estimatedTime = estimatedTime;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.causeOfAbsence = causeOfAbsence;
+    }
 
-	}
+    @Override
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    @Override
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    @Override
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    @Override
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+    
+    public ArrayList<String> getCauseOfAbsence() {
+        return causeOfAbsence;
+    }
+    
+    public void setCauseOfAbsence(ArrayList<String> causeOfAbsence) {
+        this.causeOfAbsence = causeOfAbsence;
+    }
+    
+    // add new predefined activities to the list 
+    public void addCauseOfAbsence(String newActivity) {
+        this.causeOfAbsence.add(newActivity);
+    }
+
 }
+
