841a3de6032f6045fb5b8b77bb8ccd3a236eceae
azon <32076614+AZ0N@users.noreply.github.com>
2023-05-05 12:13:25 +0200

    Remove unused step definition

--- src/test/java/example/cucumber/CreateProjectSteps.java
+++ src/test/java/example/cucumber/CreateProjectSteps.java
@@ -32,29 +32,11 @@ public class CreateProjectSteps {
 
 	@Then("the Project with id {int} and name {string} is in the system")
 	public void theProjectWithIdAndNameIsInTheSystem(int ID, String name) {
-
 		Project p = projectmanager.getProjectByID(ID);
-
 		assertNotEquals(p, null);
-
 		if (p != null) {
 			assertEquals(p.getID(), ID);
 			assertEquals(p.getProjectName(), name);
-		}
-
-	}
-
-	@Then("the Project called {string} is in the system")
-	public void theProjectCalledIsInTheSystem(String projectName) {
-		// Try to get the project with {projectName}
-		Project e = projectmanager.getProject(projectName); 
-
-		// Assert that we found an object
-		assertNotEquals(e, null);
-
-		// If we found a project, check of the names match
-		if (e != null) {
-			assertTrue(e.getProjectName().equals(projectName));
 		}
 	}
 }
