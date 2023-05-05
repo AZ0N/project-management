6e66a3dba25f986db8d816ea0eb4e95eb1330aab
azon <32076614+AZ0N@users.noreply.github.com>
2023-05-05 12:16:05 +0200

    Removed unused import

--- src/test/java/example/cucumber/CreateProjectSteps.java
+++ src/test/java/example/cucumber/CreateProjectSteps.java
@@ -2,7 +2,6 @@ package example.cucumber;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
 
 import application.projectmanagement.Project;
 import application.projectmanagement.ProjectManager;
